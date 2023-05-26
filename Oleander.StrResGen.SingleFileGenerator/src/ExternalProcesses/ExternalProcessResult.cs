3dece6fdde64e7849e21a2a6a3f8ce186172107d
Andre-Loetzsch <andre.loetzsch@live.de>
2023-05-26 15:00:37 +0200

    Check for an update every 10 days

--- Oleander.StrResGen.SingleFileGenerator/src/ExternalProcesses/ExternalProcessResult.cs
+++ Oleander.StrResGen.SingleFileGenerator/src/ExternalProcesses/ExternalProcessResult.cs
@@ -1,18 +1,38 @@
 ﻿using System.IO;
+using System.Text;
 
 namespace Oleander.StrResGen.SingleFileGenerator.ExternalProcesses
 {
     public class ExternalProcessResult
     {
-        public ExternalProcessResult(string exeFileName)
+        public ExternalProcessResult(string exeFileName, string arguments)
         {
             this.ExeFileName = Path.GetFileName(exeFileName);
+            this.CommandLine = string.Concat(exeFileName, " ", arguments);
         }
 
         public string ExeFileName { get; }
+
+        public string CommandLine { get; }
+
         public Win32ExitCodes Win32ExitCode { get; internal set; } = Win32ExitCodes.ERROR_SUCCESS;
         public int ExitCode { get; internal set; }
         public string StandardOutput { get; internal set; }
         public string StandardErrorOutput { get; internal set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("Cmd:".PadRight(20)).AppendLine(this.CommandLine);
+            sb.Append("ExeFileName:".PadRight(20)).AppendLine(this.ExeFileName);
+            sb.Append("ExitCode:".PadRight(20)).AppendLine(this.ExitCode.ToString());
+            sb.Append("Win32ExitCode:".PadRight(20)).AppendLine(this.Win32ExitCode.ToString());
+            
+            if (!string.IsNullOrEmpty(this.StandardOutput)) sb.AppendLine(this.StandardOutput);
+            if (!string.IsNullOrEmpty(this.StandardErrorOutput)) sb.AppendLine(this.StandardErrorOutput);
+
+            return sb.ToString();
+        }
     }
 }
---
+++ Oleander.StrResGen.SingleFileGenerator/src/ExternalProcesses/UpdateDotnetToolProcess.cs
@@ -0,0 +1,10 @@
+﻿namespace Oleander.StrResGen.SingleFileGenerator.ExternalProcesses
+{
+    public class UpdateDotnetToolProcess : ExternalProcess
+    {
+        public UpdateDotnetToolProcess()
+            : base("dotnet", "tool update dotnet-oleander-strresgen-tool -g --prerelease")
+        {
+        }
+    }
+}
