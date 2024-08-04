faf9477b9b57ca19b34ec88018ea47637df14eb0
André Lötzsch <andre.loetzsch@live.de>
2024-08-04 11:31:10 +0200

    Use "--property:VersionDevSuffix=dev" instead of "--version-suffix dev" to not overwrite the VersionSuffix attribute in the project file.

--- dotNetBuild.cmd
+++ dotNetBuild.cmd
@@ -1,9 +1,9 @@
 rem dotnet build StringResourceGenerator.sln --configuration Debug --version-suffix dev --property:versioningTask-disabled=true --property:NoWarn=OAVBT46
 rem dotnet pack StringResourceGenerator.sln --configuration Debug --version-suffix dev --no-restore --property:versioningTask-disabled=true --property:NoWarn=OAVBT46
 
-
 dotnet build StringResourceGenerator.sln --configuration Debug --property:VersionDevSuffix=dev --property:versioningTask-disabled=true --property:NoWarn=OAVBT46
 dotnet pack StringResourceGenerator.sln --configuration Debug --property:VersionDevSuffix=dev --no-restore --property:versioningTask-disabled=true --property:NoWarn=OAVBT46
+
 
 
 pause
