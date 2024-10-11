7e771a4ea0fc6ee5702a183910b6410a0bdf8467
Andre-Loetzsch <andre.loetzsch@live.de>
2024-10-11 15:49:29 +0200

    save status

--- Oleander.StrResGen.SingleFileGenerator/src/StrResGenCodeGenerator.cs
+++ Oleander.StrResGen.SingleFileGenerator/src/StrResGenCodeGenerator.cs
@@ -13,6 +13,10 @@ namespace Oleander.StrResGen.SingleFileGenerator
     [Guid("8C3E490C-C096-4336-B687-1162F34386E4")]
     public sealed class StrResGenCodeGenerator : BaseCodeGeneratorWithSite
     {
+
+        private readonly ResourceGenerator _generator = new();
+
+
         public int WarnLevel { get; private set; }
         public int ErrorLevel { get; private set; }
         public string Message { get; private set; }
@@ -35,93 +39,125 @@ namespace Oleander.StrResGen.SingleFileGenerator
             return result == null ? null : Encoding.UTF8.GetBytes(result);
         }
 
+
         public string GenerateCSharpCode(string inputFileName, string fileNamespace)
         {
-            if (Path.GetExtension(inputFileName) != ".strings")
-            {
-                this.CreateError(1, $"File must have '*.strings' extension! ({inputFileName})");
-                return null;
-            }
-
             if (string.IsNullOrEmpty(fileNamespace))
             {
                 this.CreateWarning(1, "File namespace is null or empty!");
             }
 
-            this._isDotnetToolInstalled = this._isDotnetToolInstalled || IsDotnetToolInstalled;
+            var result = this._generator.Generate(inputFileName, fileNamespace);
 
-            if (!this._isDotnetToolInstalled)
+            if (result != 0)
             {
-                this.CreateWarning(2, "Dotnet tool 'dotnet-oleander-strresgen-tool' not found! Try to install missing dependencies.");
-                this.ExternalProcessResult = InstallDotnetTool();
-                this._isDotnetToolInstalled = this.ExternalProcessResult.ExitCode == 0;
-            }
-            else
-            {
-                if (!this._updateDotnetToolDone && ShouldUpdateDotnetTool)
-                {
-                    this._updateDotnetToolDone = true;
-                    var result = UpdateDotnetTool();
-
-                    if (result.ExitCode != 0)
-                    {
-                        this.CreateWarning(3, $"Dotnet tool 'dotnet-oleander-strresgen-tool' update failed! {result.StandardErrorOutput}");
-                    }
-                }
-            }
-
-            this.ExternalProcessResult = (string.IsNullOrEmpty(fileNamespace) ?
-                new StrResGenProcess(inputFileName) :
-                new StrResGenProcess(inputFileName, fileNamespace)).Start();
-
-            if (this.ExternalProcessResult.Win32ExitCode == Win32ExitCodes.ERROR_FILE_NOT_FOUND)
-            {
-                this.CreateWarning(4, "Dotnet tool 'dotnet-oleander-strresgen-tool' not found! Try to install missing dependencies.");
-                this.ExternalProcessResult = InstallDotnetTool();
-
-                if (this.ExternalProcessResult.ExitCode == 0)
-                {
-                    this._isDotnetToolInstalled = true;
-                    this.ExternalProcessResult = (string.IsNullOrEmpty(fileNamespace) ?
-                        new StrResGenProcess(inputFileName) :
-                        new StrResGenProcess(inputFileName, fileNamespace)).Start();
-                }
-            }
-
-            if (this.ExternalProcessResult.ExitCode != 0)
-            {
-                if (this.ExternalProcessResult.Win32ExitCode == Win32ExitCodes.ERROR_SUCCESS)
-                {
-                    this.CreateError(2, $"Process {this.ExternalProcessResult.ExeFileName} failed!  ERROR{this.ExternalProcessResult.ExitCode} {this.ExternalProcessResult.StandardErrorOutput}");
-                }
-                else
-                {
-                    this.CreateError(3, $"Process {this.ExternalProcessResult.ExeFileName} failed!  {this.ExternalProcessResult.Win32ExitCode} {this.ExternalProcessResult.StandardErrorOutput}");
-                }
-
+                this.CreateError(result, $"StrResGen exit with exit code {result}");
                 return null;
-            }
-
-            if (this._errors.Any())
-            {
-                return string.Join(Environment.NewLine, this._errors);
             }
 
             var csFile = string.Concat(inputFileName.Substring(0, inputFileName.Length - 8), ".cs");
 
             if (!File.Exists(csFile))
             {
-                this.CreateError(4, $"File '{csFile}' not found!");
+                this.CreateError(2, $"File '{csFile}' not found!");
                 this._errors.AddRange(this._warnings);
                 return string.Join(Environment.NewLine, this._errors);
             }
 
             var csFileContent = File.ReadAllText(csFile);
 
-            return this._warnings.Any() ?
-                string.Concat(string.Join(Environment.NewLine, this._warnings), Environment.NewLine, csFileContent) :
-                csFileContent;
+            return this._warnings.Any() ? string.Concat(string.Join(Environment.NewLine, this._warnings), Environment.NewLine, csFileContent) : csFileContent;
+
         }
+
+        //public string GenerateCSharpCode1(string inputFileName, string fileNamespace)
+        //{
+
+        //    if (Path.GetExtension(inputFileName) != ".strings")
+        //    {
+        //        this.CreateError(1, $"File must have '*.strings' extension! ({inputFileName})");
+        //        return null;
+        //    }
+
+        //    if (string.IsNullOrEmpty(fileNamespace))
+        //    {
+        //        this.CreateWarning(1, "File namespace is null or empty!");
+        //    }
+
+        //    this._isDotnetToolInstalled = this._isDotnetToolInstalled || IsDotnetToolInstalled;
+
+        //    if (!this._isDotnetToolInstalled)
+        //    {
+        //        this.CreateWarning(2, "Dotnet tool 'dotnet-oleander-strresgen-tool' not found! Try to install missing dependencies.");
+        //        this.ExternalProcessResult = InstallDotnetTool();
+        //        this._isDotnetToolInstalled = this.ExternalProcessResult.ExitCode == 0;
+        //    }
+        //    else
+        //    {
+        //        if (!this._updateDotnetToolDone && ShouldUpdateDotnetTool)
+        //        {
+        //            this._updateDotnetToolDone = true;
+        //            var result = UpdateDotnetTool();
+
+        //            if (result.ExitCode != 0)
+        //            {
+        //                this.CreateWarning(3, $"Dotnet tool 'dotnet-oleander-strresgen-tool' update failed! {result.StandardErrorOutput}");
+        //            }
+        //        }
+        //    }
+
+        //    this.ExternalProcessResult = (string.IsNullOrEmpty(fileNamespace) ?
+        //        new StrResGenProcess(inputFileName) :
+        //        new StrResGenProcess(inputFileName, fileNamespace)).Start();
+
+        //    if (this.ExternalProcessResult.Win32ExitCode == Win32ExitCodes.ERROR_FILE_NOT_FOUND)
+        //    {
+        //        this.CreateWarning(4, "Dotnet tool 'dotnet-oleander-strresgen-tool' not found! Try to install missing dependencies.");
+        //        this.ExternalProcessResult = InstallDotnetTool();
+
+        //        if (this.ExternalProcessResult.ExitCode == 0)
+        //        {
+        //            this._isDotnetToolInstalled = true;
+        //            this.ExternalProcessResult = (string.IsNullOrEmpty(fileNamespace) ?
+        //                new StrResGenProcess(inputFileName) :
+        //                new StrResGenProcess(inputFileName, fileNamespace)).Start();
+        //        }
+        //    }
+
+        //    if (this.ExternalProcessResult.ExitCode != 0)
+        //    {
+        //        if (this.ExternalProcessResult.Win32ExitCode == Win32ExitCodes.ERROR_SUCCESS)
+        //        {
+        //            this.CreateError(2, $"Process {this.ExternalProcessResult.ExeFileName} failed!  ERROR{this.ExternalProcessResult.ExitCode} {this.ExternalProcessResult.StandardErrorOutput}");
+        //        }
+        //        else
+        //        {
+        //            this.CreateError(3, $"Process {this.ExternalProcessResult.ExeFileName} failed!  {this.ExternalProcessResult.Win32ExitCode} {this.ExternalProcessResult.StandardErrorOutput}");
+        //        }
+
+        //        return null;
+        //    }
+
+        //    if (this._errors.Any())
+        //    {
+        //        return string.Join(Environment.NewLine, this._errors);
+        //    }
+
+        //    var csFile = string.Concat(inputFileName.Substring(0, inputFileName.Length - 8), ".cs");
+
+        //    if (!File.Exists(csFile))
+        //    {
+        //        this.CreateError(4, $"File '{csFile}' not found!");
+        //        this._errors.AddRange(this._warnings);
+        //        return string.Join(Environment.NewLine, this._errors);
+        //    }
+
+        //    var csFileContent = File.ReadAllText(csFile);
+
+        //    return this._warnings.Any() ?
+        //        string.Concat(string.Join(Environment.NewLine, this._warnings), Environment.NewLine, csFileContent) :
+        //        csFileContent;
+        //}
 
         private void CreateWarning(int level, string message)
         {
