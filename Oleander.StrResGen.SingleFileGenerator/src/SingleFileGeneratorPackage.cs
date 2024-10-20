c4c130bbc5912cf5b60c7eb3d902a71f7362d908
André Lötzsch <andre.loetzsch@live.de>
2024-10-20 21:44:53 +0200

    save status

--- Oleander.StrResGen.SingleFileGenerator/src/SingleFileGeneratorPackage.cs
+++ Oleander.StrResGen.SingleFileGenerator/src/SingleFileGeneratorPackage.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
-using System;
 using System.Runtime.InteropServices;
-using System.Threading;
 using Task = System.Threading.Tasks.Task;
+using System;
+using System.Threading;
+using System.IO;
 
-namespace Oleander.StrResGen.SingleFileGenerator
-{
+namespace Oleander.StrResGen.SingleFileGenerator;
+
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(StrResGenSingleFileGeneratorPackage.PackageGuidString)]
     [ProvideCodeGenerator(typeof(StrResGenCodeGenerator), "SRG", "string resource generator", true)]
@@ -14,13 +15,30 @@ namespace Oleander.StrResGen.SingleFileGenerator
     {
         public const string PackageGuidString = "40bd30fb-a6b5-4183-8c78-a5861a267465";
 
-        #region Package Members
-   
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        public StrResGenSingleFileGeneratorPackage()
         {
+            Log.Write("StrResGenSingleFileGeneratorPackage()");
+        }
+
+    #region Package Members
+
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            Log.Write("InitializeAsync");
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
         }
 
         #endregion
     }
-}
+
+    internal static class Log
+    {
+        internal static void Write(string text)
+        {
+            var path = Path.Combine(Path.GetTempPath(), "srg.log");
+            //if (!File.Exists(path)) return;
+
+            File.AppendAllText(path, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {text}{Environment.NewLine}");
+        }
+    }
+
